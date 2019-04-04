aa23f52e147b3ee47dacea1ee0cdc41a7a530752
pete rodrigue <edwardpierrerodrigue@gmail.com>
2019-04-03 22:28:45 -0500

    progress on part 1

--- hw1.py
+++ hw1.py
@@ -5,31 +5,22 @@ pete rodrigue
 '''
 
 import pandas as pd
-from sodapy import Socrata
 
-# Uncomment the code below to download the crimes data again.
-# Otherwise skip down to the part where we load the crimes data from the repo.
 
-'''
-# Much of the code below comes from the documentation on the
-# city of chicago api docs webpage:
-# https://dev.socrata.com/foundry/data.cityofchicago.org/6zsd-86xi
+# Part one
 
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
-client = Socrata("data.cityofchicago.org", None)
+results_2017 = pd.read_json(
+    'https://data.cityofchicago.org/resource/crimes.json?year=2017')
+results_2018 = pd.read_json(
+    'https://data.cityofchicago.org/resource/crimes.json?year=2018')
 
-# First 2000 results, returned as JSON from API / converted to Python list of
-# dictionaries by sodapy.
-results = client.get("6zsd-86xi"  # , limit=10
-                     )
-
-# Convert to pandas DataFrame
-results_df = pd.DataFrame.from_records(results)
+results_df = results_2017.append(results_2018, ignore_index=True)
 results_df.to_csv('alleged_crimes_data.csv', index=False)
-'''
 
 df = pd.read_csv('alleged_crimes_data.csv')
-print(df.head(15))
-print(df.shape)
-print(df.columns)
+
+# Number of crimes of each type
+
+# How they change over time
+
+# How they are different by neighborhood
