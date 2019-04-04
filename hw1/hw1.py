c374d2a9b4466bacdbfa6a009cce817abfb36da4
pete rodrigue <edwardpierrerodrigue@gmail.com>
2019-04-04 17:19:36 -0500

    working on map

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -7,6 +7,7 @@ pete rodrigue
 import pandas as pd
 import seaborn as sns
 import json
+import folium
 from bokeh.models import GeoJSONDataSource
 from bokeh.models import GeoJSONDataSource, LogColorMapper
 from bokeh.palettes import Viridis6 as palette
@@ -71,6 +72,7 @@ a = sns.catplot(y="type of crime", x="count", hue="year",
 a.set_xlabels("Number of reported crimes")
 axes_a = a.axes.flatten()
 axes_a[0].set_title('Common crimes (at least 2,500 reported instances)')
+a.savefig("hw1/common crimes.png")
 
 b = sns.catplot(y="type of crime", x="count", hue="year",
                 data=df.loc[df['count'] < 2500, :],
@@ -78,9 +80,10 @@ b = sns.catplot(y="type of crime", x="count", hue="year",
 b.set_xlabels("Number of reported crimes")
 axes_b = b.axes.flatten()
 axes_b[0].set_title('Less common crimes (less than 2,500 reported instances)')
+b.savefig("hw1/not common crimes.png")
 
 # How they are different by neighborhood
-
+'''
 df = df_2017.append(df_2018, ignore_index=True)
 df = df.groupby(['year', 'community_area', 'primary_type']).size().reset_index()
 df.columns = ['year', 'community_area', 'type of crime', 'count']
@@ -116,3 +119,76 @@ p.patches('xs', 'ys', fill_alpha=0.7,
 outfile = open('map_of_neighborhoods.html', 'w')
 outfile.write(file_html(p, CDN, 'reported crime'))
 outfile.close()
+'''
+
+
+
+
+
+
+df_2017 = pd.read_csv('alleged_crimes_2017.csv')
+df_2018 = pd.read_csv('alleged_crimes_2018.csv')
+
+df_2017.head()
+df = df_2017.groupby(['year', 'community_area', 'primary_type']).size().reset_index()
+df.columns = ['year', 'community_area', 'type of crime', 'count']
+
+community_area_names = pd.read_csv('hw1/CommAreas.csv')
+community_area_names.head()
+community_area_names = community_area_names[['AREA_NUMBE', 'COMMUNITY']]
+df = df.merge(
+    community_area_names,
+    how='left', left_on='community_area', right_on='AREA_NUMBE')
+df = df.dropna(subset=['AREA_NUMBE'])
+
+grouped_df = df.groupby(['COMMUNITY', 'type of crime']).sum().reset_index()[['COMMUNITY', 'type of crime', 'count']].sort_values('count', ascending=False)
+grouped_df.head()
+wide_df = grouped_df.pivot(index='COMMUNITY', columns='type of crime', values='count').reset_index()
+wide_df.fillna(0)
+wide_df.head()
+
+
+g = sns.PairGrid(wide_df.sort_values("THEFT", ascending=False),
+                 x_vars=['THEFT', 'BATTERY', 'CRIMINAL DAMAGE', 'ASSAULT'], y_vars=["COMMUNITY"],
+                 height=10, aspect=.25)
+
+g.map(sns.stripplot, size=10, orient="h",
+      palette="ch:s=1,r=-.1,h=1_r", linewidth=1, edgecolor="w")
+
+g.set(xlabel="# reported crimes", ylabel="")
+
+# Use semantically meaningful titles for the columns
+titles = ['THEFT', 'BATTERY', 'CRIMINAL DAMAGE', 'ASSAULT']
+
+for ax, title in zip(g.axes.flat, titles):
+
+    # Set a different title for each axes
+    ax.set(title=title)
+
+    # Make the grid horizontal instead of vertical
+    ax.xaxis.grid(False)
+    ax.yaxis.grid(True)
+
+sns.despine(left=True, bottom=True)
+plt.gcf().set_size_inches(16, 8.27)
+g.savefig('hw1/reported crimes by community area.png')
+
+
+
+
+# mapping with folium
+chi_map = folium.Map(location=[41.860909, -87.630780], tiles='Stamen Toner', zoom_start=10)
+# creation of the choropleth
+with open("hw1/Boundaries - Community Areas (current) (1).geojson") as f:
+    geodata = json.load(f)
+chi_map.geo_json(geo_path = district_geo,
+              data_out = 'crimeagg.json',
+              data = crimedata2,
+              columns = ['District', 'Number'],
+              key_on = 'feature.properties.DISTRICT',
+              fill_color = 'YlOrRd',
+              fill_opacity = 0.7,
+              line_opacity = 0.2,
+              legend_name = 'Number of incidents per district')
+
+chi_map.save('hw1/map.html')
